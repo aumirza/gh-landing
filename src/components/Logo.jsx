de66b75761fdae63a4d24026be8679be1efc5eef
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-01-24 20:53:48 +0530

    feat: adjust logo font size for improved responsiveness

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center">
       <FaGithub className="text-2xl" />
-      <span className="ml-2 font-mono text-2xl font-bold">
+      <span className="ml-2 font-mono text-lg font-bold md:text-2xl">
         {
           // first letter capital
           username.charAt(0).toUpperCase() + username.slice(1)
